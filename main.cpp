99f0d9c262adef8b4282c43e8772a61fd78029ae
Loma <lomakim@gmail.com>
2026-04-05 21:50:10 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <set>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
-    
+    ifstream fin("codes.txt");
+    vector<string> vec;
+    list<string> list;
+    set<string> set;
+    string temp;
+
+
+    auto start = high_resolution_clock::now();
+    while (fin >> temp) {
+        vec.push_back(temp);
+    }
+    auto end = high_resolution_clock::now();
+
+    auto vRead = duration_cast<milliseconds>(end - start);
+
+    fin.close();
 
     return 0;
 }
