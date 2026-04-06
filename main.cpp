fa84ab2e5679aad8c95f03aca9e7c4287c32177b
Loma <lomakim@gmail.com>
2026-04-05 22:00:50 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <list>
 #include <set>
+#include <algorithm>
 using namespace std;
 using namespace std::chrono;
 
@@ -14,16 +15,40 @@ int main() {
     set<string> set;
     string temp;
 
-
+    // READING FILE
     auto start = high_resolution_clock::now();
     while (fin >> temp) {
         vec.push_back(temp);
     }
     auto end = high_resolution_clock::now();
-
     auto vRead = duration_cast<milliseconds>(end - start);
 
+    start = high_resolution_clock::now();
+    while (fin >> temp) {
+        list.push_back(temp);
+    }
+    end = high_resolution_clock::now();
+    auto lRead = duration_cast<milliseconds>(end - start);
+    
+    start = high_resolution_clock::now();
+    while (fin >> temp) {
+        set.insert(temp);
+    }
+    end = high_resolution_clock::now();
+    auto sRead = duration_cast<milliseconds>(end - start);
+
     fin.close();
+
+    //SORTING FILE
+    start = high_resolution_clock::now();
+    sort(vec.begin(), vec.end());
+    end = high_resolution_clock::now();
+    auto vSort = duration_cast<milliseconds>(end - start);
+
+    start = high_resolution_clock::now();
+    end = high_resolution_clock::now();
+
+    duration_cast<milliseconds>(end - start);
 
     return 0;
 }
